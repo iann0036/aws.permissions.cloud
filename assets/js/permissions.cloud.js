e8ededc90be36a85ce71c23960d973b77e4dc4fe
Ian Mckay <contact@ianmckay.com.au>
2021-05-23 12:49:10 +1000

    Tags

--- assets/js/permissions.cloud.js
+++ assets/js/permissions.cloud.js
@@ -1,27 +1,69 @@
 // permissions.cloud Core Functionality
 
+function arnReplace(arn) {
+    arn = arn.replace(/\$\{(Partition)\}/g, '<span class="tx-semibold tx-gray-500">aws</span>');
+    arn = arn.replace(/\$\{(Region)\}/g, '<span class="tx-semibold tx-gray-500">us-east-1</span>');
+    arn = arn.replace(/\$\{(Account)\}/g, '<span class="tx-semibold tx-gray-500">123456789012</span>');
+    arn = arn.replace(/\$\{(.+?)\}/g, '<a class="tx-semibold" href="#" data-toggle="modal" data-target="#resourceTypeModal">$1</a>');
+
+    return arn;
+}
+
+function overrideReplace(arn) {
+    if (arn.includes("%%iftruthy%")) {
+        let arn_parts = arn.split("%");
+        let else_ext = '';
+        if (arn_parts[5] != "") {
+            else_ext = ' <span class="badge badge-info">otherwise</span> ' + arnReplace(arn_parts[5]);
+        }
+
+        return arnReplace(arn_parts[0]) + '<span class="badge badge-info">if exists</span> ' + arnReplace(arn_parts[3]) + ' <span class="badge badge-info">then</span> ' + arnReplace(arn_parts[4]) + else_ext + ' <span class="badge badge-dark">overriden</span>';
+    } else if (arn.includes("%%many%")) {
+        let arn_parts = arn.split("%");
+        let many_instances = [];
+        for (let i=3; i<arn_parts.length-2; i++) {
+            many_instances.push(arnReplace(arn_parts[i]));
+        }
+
+        return arnReplace(arn_parts[0]) + many_instances.join("<br />") + ' <span class="badge badge-dark">overriden</span>';
+    } else if (arn.includes("%%urlencode%")) {
+        let arn_parts = arn.split("%");
+
+        return arnReplace(arn_parts[0]) + '<span class="badge badge-info">urlencode</span> ' + arnReplace(arn_parts[3]) + ' <span class="badge badge-dark">overriden</span>';
+    } else if (arn.includes("%%iftemplatematch%")) {
+        let arn_parts = arn.split("%");
+
+        return arnReplace(arn_parts[0]) + '<span class="badge badge-info">if ARN matches format</span> ' + arnReplace(arn_parts[3]) + ' <span class="badge badge-dark">overriden</span>';
+    } else if (arn.includes("%%regex%")) {
+        let arn_parts = arn.split("%");
+
+        return arnReplace(arn_parts[0]) + '<span class="badge badge-info">for the property</span> ' + arnReplace(arn_parts[3]) + ' <span class="badge badge-info">get $1 of the regex pattern</span> ' + arn_parts[4] + ' <span class="badge badge-dark">overriden</span>';
+    }
+
+    return arnReplace(arn) + ' <span class="badge badge-dark">overriden</span>';
+}
+
 async function getTemplates(action, iam_def) {
-    let action_parts = action.split(":");
+    let action_parts = action['action'].split(":");
     let ret = '*';
     let templates = [];
 
-    for (let service_def of iam_def) {
-        if (service_def['prefix'] == action_parts[0]) {
-            for (let privilege of service_def['privileges']) {
-                if (privilege['privilege'] == action_parts[1]) {
-                    for (let resource_type of privilege['resource_types']) {
-                        if (resource_type['resource_type'] != "") {
-                            resource_type_name = resource_type['resource_type'].replace("*", "");
-                            for (let resource of service_def['resources']) {
-                                if (resource['resource'] == resource_type_name) {
-                                    let arn = resource['arn'];
+    if (action['arn_override']) {
+        templates.push(overrideReplace(action['arn_override']['template']));
+    } else {
+        for (let service_def of iam_def) {
+            if (service_def['prefix'] == action_parts[0]) {
+                for (let privilege of service_def['privileges']) {
+                    if (privilege['privilege'] == action_parts[1]) {
+                        for (let resource_type of privilege['resource_types']) {
+                            if (resource_type['resource_type'] != "") {
+                                resource_type_name = resource_type['resource_type'].replace("*", "");
+                                for (let resource of service_def['resources']) {
+                                    if (resource['resource'] == resource_type_name) {
+                                        let arn = arnReplace(resource['arn']);
 
-                                    arn = arn.replace(/\$\{(Partition)\}/g, '<span class="tx-semibold tx-gray-500">aws</span>');
-                                    arn = arn.replace(/\$\{(Region)\}/g, '<span class="tx-semibold tx-gray-500">us-east-1</span>');
-                                    arn = arn.replace(/\$\{(Account)\}/g, '<span class="tx-semibold tx-gray-500">123456789012</span>');
-                                    arn = arn.replace(/\$\{(.+?)\}/g, '<span class="tx-semibold tx-info">$1</span>');
-
-                                    templates.push(arn);
+                                        templates.push(arn);
+                                    }
                                 }
                             }
                         }
@@ -50,13 +92,15 @@ async function getUsedBy(privilege, sdk_map) {
     }
 
     if (used_by_methods.length) {
+        used_by_methods.sort();
+
         return used_by_methods.join("<br />");
     }
 
     return '-';
 }
 
-async function preprocess() {
+async function processReferencePage() {
     let iam_def_data = await fetch('/iam_definition.json');
     let iam_def = await iam_def_data.json();
     let service = iam_def[0];
@@ -172,7 +216,7 @@ async function preprocess() {
             let rowspan = sdk_map['sdk_method_iam_mappings'][iam_mapping_name].length + 1;
 
             let actionlink = "/iam/" + first_action['action'].split(":")[0];
-            let template = await getTemplates(first_action['action'], iam_def);
+            let template = await getTemplates(first_action, iam_def);
 
             method_table_content += '<tr>\
                 <td rowspan="' + rowspan + '" class="tx-medium"><span class="tx-color-03">' + iam_mapping_name_parts[0] + '.</span>' + iam_mapping_name_parts[1] + '</td>\
@@ -183,7 +227,7 @@ async function preprocess() {
 
             for (let action of sdk_map['sdk_method_iam_mappings'][iam_mapping_name]) {
                 let actionlink = "/iam/" + action['action'].split(":")[0];
-                let template = await getTemplates(action['action'], iam_def);
+                let template = await getTemplates(action, iam_def);
 
                 method_table_content += '<tr>\
                     <td class="tx-medium" style="padding-left: 10px !important;"><a href="' + actionlink + '">' + action['action'] + '</a></td>\
@@ -195,8 +239,8 @@ async function preprocess() {
         }
     }
 
-    $('#api-count').html(api_count);
+    $('#api-count').html(api_count.toString());
     $('#methods-table tbody').append(method_table_content);
 }
 
-preprocess();
+processReferencePage();
